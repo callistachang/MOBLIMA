407b950c1bdc2862675a093de899b400a77ba509
Penelope Chua <50069538+penelopechua@users.noreply.github.com>
2019-11-10 00:46:49 +0800

    Added function skeletons
    - Main functions of CinemaStaffApp and GuestApp

--- src/managers/CineplexManager.java
+++ src/managers/CineplexManager.java
@@ -11,4 +11,14 @@ public class CineplexManager {
 	public int listAll() {
 		return 0; //to change code accordingly  
 	}
+	
+	public int addCineplex() {
+		return 0; //similar to create
+	}
+	
+	public int removeCineplex() {
+		return 0; //how to choose correct cineplex?
+	}
+	
+	
 }
