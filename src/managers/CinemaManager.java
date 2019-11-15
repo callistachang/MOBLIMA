9b23ed2258a63a0f68ce60e27ebc11919279fc63
callistachang <rezinomrah@gmail.com>
2019-11-15 19:40:45 +0800

    update all managers to have the database

--- src/managers/CinemaManager.java
+++ src/managers/CinemaManager.java
@@ -2,12 +2,27 @@ package managers;
 
 import java.util.ArrayList;
 
+import handlers.DatabaseHandler;
+import models.Account;
+import models.Booking;
 import models.Cinema;
 import models.Movie;
 import models.Showtime;
+import serializers.AbstractSerializer;
+import serializers.CinemaSerializer;
 
 public class CinemaManager {
+	private static final String DATABASE_NAME = "cinemadata";
 	private static ArrayList<Cinema> records = null;
+	
+	public Cinema getCinemaByID(int cinemaID) {
+		for (Cinema c: records) {
+			if (c.getId() == cinemaID) {
+				return c;
+			}
+		}
+		return null;
+	}
 	
 	public void listAllByCineplex(int cineplexID) {
 		
@@ -48,6 +63,17 @@ public class CinemaManager {
 //			sm.getShowtimeMovie(showtime,movie)
 //			
 //			sm.getShowtimes(movieID);
-		}
+	}
+
+	private static void initializeDatabase() {
+		ArrayList<String> data = DatabaseHandler.readDatabase(DATABASE_NAME);
+		AbstractSerializer serializer = new CinemaSerializer();
+		records = serializer.deserialize(data);
+	}
+	
+	private void updateDatabase() {
+		AbstractSerializer serializer = new CinemaSerializer();
+		ArrayList<String> updatedRecords = serializer.serialize(records);
+		DatabaseHandler.writeToDatabase(DATABASE_NAME, updatedRecords);
 	}
 }
--- src/managers/ReviewManager.java
+++ src/managers/ReviewManager.java
@@ -1,6 +1,12 @@
 package managers;
 import java.util.ArrayList;
+
+import handlers.DatabaseHandler;
+import models.Movie;
 import models.Review;
+import serializers.AbstractSerializer;
+import serializers.ReviewSerializer;
+import serializers.ShowtimeSerializer;
 
 public class ReviewManager {
 	private static final String DATABASE_NAME = "reviewdata";
@@ -15,11 +21,32 @@ public class ReviewManager {
 		return DATABASE_NAME;
 	}
 	
+	public Review getReviewByID(int reviewID) {
+		for (Review r: records) {
+			if (r.getId() == reviewID) {
+				return r;
+			}
+		}
+		return null;
+	}
+	
 	public ArrayList<Review> getReviews(ArrayList<Integer> id){
 		int i;
 		for (i=0; i<id.size();i++) {
 			//get the reviews from database, which matches the id
 			//System.out.println(Review.getReview());
 		}
+	}
+	
+	private static void initializeDatabase() {
+		ArrayList<String> data = DatabaseHandler.readDatabase(DATABASE_NAME);
+		AbstractSerializer serializer = new ReviewSerializer();
+		records = serializer.deserialize(data);
+	}
+	
+	private void updateDatabase() {
+		AbstractSerializer serializer = new ReviewSerializer();
+		ArrayList<String> updatedRecords = serializer.serialize(records);
+		DatabaseHandler.writeToDatabase(DATABASE_NAME, updatedRecords);
 	}
 }
