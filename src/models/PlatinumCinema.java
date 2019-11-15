3a70cef33f121348de7eb97ef8e850fd11b41f55
callistachang <rezinomrah@gmail.com>
2019-11-15 19:58:12 +0800

    created cinema and subclasses

---
+++ src/models/PlatinumCinema.java
@@ -0,0 +1,24 @@
+package models;
+
+import java.util.ArrayList;
+
+public class PlatinumCinema extends Cinema {
+
+	private static final int NUM_ROWS = 5;
+	private static final int NUM_COLS = 10;
+	
+	public PlatinumCinema(int id, ArrayList<Showtime> showtimes) {
+		super(id, showtimes);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public ArrayList<Object> getSerializableData() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	public String getCinemaClass() {
+		return "Platinum";
+	}
+}
