7b13a8d5e1caad630ddba815f2e37b5f5355eb3d
DarshiniBalamurugan <bala.darshini@gmail.com>
2019-11-18 11:48:43 +0800

    javadoc...final?

---
+++ src/managers/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Act as controllers between the boundaries and entities.
+ * Contain the main logic of the application.
+ * <p>
+ * @author balad
+ * @version 1.0
+ * @since 2019-11-17
+ */
+package managers;
